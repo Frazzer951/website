3ab5db6ae3809b5230dea4039d77a4106cf9357c
Luke Eltiste <luke343279@gmail.com>
2023-03-04 18:34:17 -0800

    setup dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM rust:latest as build
+
+RUN rustup target add wasm32-unknown-unknown
+RUN cargo install trunk wasm-bindgen-cli
+
+WORKDIR /usr/src/website
+COPY . .
+
+RUN cd frontend && \
+    trunk build --release
+RUN cargo build --release
+
+FROM gcr.io/distroless/cc-debian10
+
+COPY --from=build /usr/src/website/target/release/website /usr/local/bin/backend
+COPY --from=build /usr/src/website/frontend/dist /usr/local/bin/dist
+
+WORKDIR /usr/local/bin
+CMD [ "backend" ]
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,7 @@ mod api;
 use api::test::test;
 
 use actix_web::{middleware::Logger, web::scope, App, HttpServer};
+use actix_web_lab::web::spa;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,7 +13,16 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let logger = Logger::default();
-        App::new().wrap(logger).service(scope("/api").service(test))
+        App::new()
+            .wrap(logger)
+            .service(scope("/api").service(test))
+            .service(
+                spa()
+                    .index_file("./dist/index.html")
+                    .static_resources_mount("/")
+                    .static_resources_location("./dist")
+                    .finish(),
+            )
     })
     .bind(("0.0.0.0", 80))?
     .run()
