2931ebeddc5833fb224926b1d993fb1b337dae94
Luke Eltiste <luke343279@gmail.com>
2023-03-07 10:57:26 -0800

    Console logging

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,7 +1,24 @@
+use gloo::console::log;
+use serde::{Deserialize, Serialize};
 use yew::prelude::*;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct MyObject {
+    username: String,
+    favorite_language: String,
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let name = "Luke";
+    let my_object = MyObject {
+        username: name.to_string(),
+        favorite_language: "Rust".to_string(),
+    };
+
+    log!("My name is", name);
+    log!(serde_json::to_string_pretty(&my_object).unwrap());
+
     html! {
     <h1> {"Hello World!"} </h1>
     }
