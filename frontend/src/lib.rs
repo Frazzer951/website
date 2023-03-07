d26a901d869f1481d00b0d2da7e6361b9e759738
Luke Eltiste <luke343279@gmail.com>
2023-03-07 11:50:48 -0800

    Clean Slate

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,15 +1,8 @@
-use stylist::{style, yew::styled_component};
 use yew::prelude::*;
 
-#[styled_component(App)]
+#[function_component(App)]
 pub fn app() -> Html {
-    let stylesheet = style!(
-        r#"
-            color: red;
-        "#
-    ).unwrap();
-
     html! {
-        <h1 class={stylesheet}> {"Hello World!"} </h1>
+        <h1> {"Hello World!"} </h1>
     }
 }
