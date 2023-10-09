1d83029e1b7a6c330b7ad5c33ef7a6922a0d1ad0
Luke Eltiste <luke343279@gmail.com>
2023-10-08 19:40:42 -0700

    clean backend

--- backend/src/api/test.rs
+++
@@ -1,6 +0,0 @@
-use actix_web::{get, web::Json};
-
-#[get("/test")]
-pub async fn test() -> Json<String> {
-    Json("test".to_string())
-}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,21 @@
-mod api;
-
-use api::test::test;
-
-use actix_web::{middleware::Logger, web::scope, App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
 use actix_web_lab::web::spa;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_LOG", "info");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
     HttpServer::new(move || {
         let logger = Logger::default();
-        App::new()
-            .wrap(logger)
-            .service(scope("/api").service(test))
-            .service(
-                spa()
-                    .index_file("./dist/index.html")
-                    .static_resources_mount("/")
-                    .static_resources_location("./dist")
-                    .finish(),
-            )
+        App::new().wrap(logger).service(
+            spa()
+                .index_file("./dist/index.html")
+                .static_resources_mount("/")
+                .static_resources_location("./dist")
+                .finish(),
+        )
     })
     .bind(("0.0.0.0", 80))?
     .run()
